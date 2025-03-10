27bff73e91758263e4bba3a2f246f8d04a5d2fba
Matthieu Bé <shmurtzy@gmail.com>
2025-03-10 12:18:53 +0100

    Ajout de la page d'édition pour NotificationSetting

---
+++ app/Filament/Resources/NotificationSettingResource/Pages/EditNotificationSetting.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Filament\Resources\NotificationSettingResource\Pages;
+
+use App\Filament\Resources\NotificationSettingResource;
+use Filament\Actions;
+use Filament\Notifications\Notification;
+use Filament\Resources\Pages\EditRecord;
+
+class EditNotificationSetting extends EditRecord
+{
+    protected static string $resource = NotificationSettingResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+    
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+    
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Notification setting updated')
+            ->body('The notification setting has been updated successfully.');
+    }
+}
